873b3a5886367392db0653f3366ed265695948f1
Olli Väisänen <olli.vaisanen@iki.fi>
2020-06-09 20:20:09 +0300

    Bouncing Balls and Evil Circle
    JavaScript OOJS assessment: add player controlled Evil Circle that can eat the balls

--- javascript/oojs/assessment/main.js
+++ javascript/oojs/assessment/main.js
@@ -1,4 +1,5 @@
 // setup canvas
+let para = document.querySelector('p');
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -57,7 +58,7 @@ class Ball extends Shape {
 
   collisionDetect() {
     for (let j = 0; j < balls.length; j++) {  //loop through all balls
-      if(!(this === balls[j])) {              //but skip this one
+      if(!(this === balls[j]) && balls[j].exists) {
         const dx = this.x - balls[j].x;
         const dy = this.y - balls[j].y;
         const distance = Math.sqrt(dx * dx + dy * dy);  //sqrt(dx^2 + dy^2)
@@ -68,6 +69,67 @@ class Ball extends Shape {
           // balls[j].velY = -(balls[j].velY); //make balls bounce off of each other
           // this.velX = -(this.velX);         //balls still sometimes get stuck together
           // this.velY = -(this.velY);
+        }
+      }
+    }
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y, exists) {
+    super(x, y, 20, 20, exists);
+    this.color = 'white';
+    this.size = 10;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if ((this.x + this.size) >= width) {
+      this.x -= this.size;
+    }
+    if ((this.x - this.size) <= 0) {
+      this.x += this.size;
+    }
+    if ((this.y + this.size) >= height) {
+      this.y -= this.size;
+    }
+    if ((this.y - this.size) <= 0) {
+      this.y += this.size;
+    }
+
+  }
+
+  setControls() {
+    let _this = this;
+    window.onkeydown = function(e) {
+      if (e.key === 'a') {
+        _this.x -= _this.velX;
+      } else if (e.key === 'd') {
+        _this.x += _this.velX;
+      } else if (e.key === 'w') {
+        _this.y -= _this.velY;
+      } else if (e.key === 's') {
+        _this.y += _this.velY;
+      }
+    }
+  }
+
+  collisionDetect() {
+    for (let j = 0; j < balls.length; j++) {  //loop through all balls
+      if(balls[j].exists) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);  //sqrt(dx^2 + dy^2)
+      
+        if (distance < this.size + balls[j].size) {
+          balls[j].exists = false;
         }
       }
     }
@@ -90,19 +152,34 @@ while (balls.length < 25) {
     'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')', //color
     size
   );
-
   balls.push(ball);
 }
+
+let evilCircle = new EvilCircle(
+  random(10, width - 10), //x
+  random(10, height - 10), //y
+  true);
+evilCircle.setControls();
 
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
+  let count = 0;
   for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if (balls[i].exists) {
+      count += 1;
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+    
   }
+  para.textContent = 'Ball count: ' + count;
 
   requestAnimationFrame(loop);
 }
